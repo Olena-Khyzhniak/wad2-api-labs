9d5ad166ae88a94c5310e309618114f9007e138c
Olena-Khyzhniak <20106012@mail.wit.ie>
2025-11-11 20:42:50 +0000

    Express Lab - exercise done

--- tasky-api/api/tasks/index.js
+++ tasky-api/api/tasks/index.js
@@ -24,13 +24,16 @@ router.get('/:id', (req, res) => {
 //Add a task
 router.post('/', (req, res) => {
     const { title, description, deadline, priority, done } = req.body;
+    const timestamp = new Date().toISOString();
     const newTask = {
         id: uuidv4(),
         title,
         description,
         deadline,
         priority,
-        done
+        done,
+        created_at: timestamp,
+        updated_at: timestamp
     };
     tasksData.tasks.push(newTask);
     res.status(201).json(newTask);
@@ -45,7 +48,16 @@ router.put('/:id', (req, res) => {
     if (taskIndex === -1) {
         return res.status(404).json({ status: 404, message: 'Task not found' });
     }
-    const updatedTask = { ...tasksData.tasks[taskIndex], ...req.body, id:id };
+
+    const oldTask = tasksData.tasks[taskIndex];
+
+    const updatedTask = { 
+        ...oldTask,
+        
+        ...req.body, 
+        id:id,
+        created_at: oldTask.created_at,
+        updated_at: new Date().toISOString()};
     tasksData.tasks[taskIndex] = updatedTask;
     res.json(updatedTask);
 });
