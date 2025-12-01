dad953370dbb096f00c95178f308975c7e8a447d
Olena-Khyzhniak <20106012@mail.wit.ie>
2025-12-01 14:04:33 +0000

    Added Auth Middleware

--- tasky-api/index.js
+++ tasky-api/index.js
@@ -4,7 +4,7 @@ import express from 'express';
 import tasksRouter from './api/tasks';
 import cors from 'cors';
 import usersRouter from './api/users';
-
+import authenticate from './authenticate';
 
 
 dotenv.config();
@@ -29,7 +29,7 @@ app.use(cors());
 
 app.use(express.json());
 
-app.use('/api/tasks', tasksRouter);
+app.use('/api/tasks', authenticate, tasksRouter);
 
 app.use('/api/users', usersRouter);
 
